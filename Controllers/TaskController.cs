5aab93669ac37300b0c459d11ba20046aeeb7342
Katlicia <mama-4153@hotmail.com>
2024-11-04 14:38:14 +0300

    edit feature added (not complete)

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -64,5 +64,57 @@ namespace TodoApp.Controllers
 
             return View(model);
         }
+
+        public IActionResult Edit(int id)
+        {
+            var task = _context.Tasks.FirstOrDefault(t => t.Id == id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            var model = new TaskViewModel
+            {
+                Title = task.Title,
+                Description = task.Description,
+                DueDate = task.DueDate
+            };
+
+            ViewData["TaskId"] = task.Id;
+
+            return View(model);
+        }
+
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Edit(int id, TaskViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var task = _context.Tasks.FirstOrDefault(t => t.Id == id);
+                if (task == null)
+                {
+                    return NotFound();
+                }
+
+                task.Title = model.Title;
+                task.Description = model.Description;
+                task.DueDate = model.DueDate;
+
+                try
+                {
+                    _context.Tasks.Update(task);
+                    _context.SaveChanges();
+                    TempData["Success Message"] = $"{task.Title} task updated successfully.";
+                    return RedirectToAction("Index");
+                }
+                catch (DbUpdateException)
+                {
+                    ModelState.AddModelError("", "An error occured while updating the task.");
+                }
+            }
+
+            return View(model);
+        }
     }
 }
